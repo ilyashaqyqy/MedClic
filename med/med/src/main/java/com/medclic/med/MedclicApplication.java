be40d3efa56d8ffd989678b568f32dfee3be6840
ilyashq <ilyasenaa24@gmail.com>
2024-08-27 11:30:48 +0100

    Création de la structure initiale du projet

---
+++ med/med/src/main/java/com/medclic/med/MedclicApplication.java
@@ -0,0 +1,13 @@
+package com.medclic.med;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedclicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedclicApplication.class, args);
+	}
+
+}
