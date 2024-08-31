55e470705dc1881d277c7fa09fc302cc9fa8e952
ilyashq <ilyasenaa24@gmail.com>
2024-08-31 21:48:27 +0100

    Ajout du contrôleur PatientController pour la gestion des patients

---
+++ med/med/src/main/java/com/medclic/med/controller/PatientController.java
@@ -0,0 +1,53 @@
+package com.medclic.med.controller;
+
+
+
+import com.medclic.med.dto.PatientDTO;
+
+import com.medclic.med.service.PatientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/patients")
+public class PatientController {
+
+    @Autowired
+    private PatientService patientService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PatientDTO> getPatientById(@PathVariable Long id) {
+        PatientDTO patientDTO = patientService.getPatientById(id);
+        return new ResponseEntity<>(patientDTO, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<PatientDTO> createPatient(@RequestBody PatientDTO patientDTO) {
+        PatientDTO createdPatient = patientService.createPatient(patientDTO);
+        return new ResponseEntity<>(createdPatient, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<PatientDTO> updatePatient(@PathVariable Long id, @RequestBody PatientDTO patientDTO) {
+        patientDTO.setId(id);
+        PatientDTO updatedPatient = patientService.updatePatient(patientDTO);
+        return new ResponseEntity<>(updatedPatient, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletePatient(@PathVariable Long id) {
+        patientService.deletePatient(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<PatientDTO>> getAllPatients() {
+        List<PatientDTO> patients = patientService.getAllPatients();
+        return new ResponseEntity<>(patients, HttpStatus.OK);
+    }
+}
+
