6a7542ebdd1d270714729ce732c60eb0a1e741ba
ilyashq <ilyasenaa24@gmail.com>
2024-09-08 14:15:49 +0100

    Ajout des composants Login et Register pour l'authentification des utilisateurs

---
+++ MedClic-frontend/src/app/auth/register/register.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { RegisterRequest } from '../../models/register.model';
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  registerForm!: FormGroup;
+
+  constructor(private fb: FormBuilder, private authService: AuthService, private router: Router) { }
+
+  ngOnInit(): void {
+    this.registerForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      username: ['', [Validators.required, Validators.minLength(4)]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  register(): void {
+    if (this.registerForm.valid) {
+      this.authService.register(this.registerForm.value).subscribe({
+        next: (response) => {
+          console.log('Registration successful', response);
+          this.router.navigate(['/login']); // Redirect to login
+        },
+        error: (error) => {
+          console.error('Registration failed', error);
+        }
+      });
+    } else {
+      console.log('Form is invalid');
+    }
+  }
+
+  get email() {
+    return this.registerForm.get('email');
+  }
+
+  get username() {
+    return this.registerForm.get('username');
+  }
+
+  get password() {
+    return this.registerForm.get('password');
+  }
+}
