dac862351d034cfff8673936ed8be6e61f4ef7fb
ilyashq <ilyasenaa24@gmail.com>
2024-10-05 22:03:11 +0100

    create docker-compose.yml et Dockerfile

--- med/med/src/main/java/com/medclic/med/repository/DoctorRepository.java
+++ med/med/src/main/java/com/medclic/med/repository/DoctorRepository.java
@@ -1,16 +1,20 @@
 package com.medclic.med.repository;
 
+import com.medclic.med.model.Appointment;
 import com.medclic.med.model.Doctor;
 import com.medclic.med.model.Location;
 import com.medclic.med.model.Specialization;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Repository
 public interface DoctorRepository extends JpaRepository<Doctor, Long> {
     List<Doctor> findBySpecializationsContaining(Specialization specialization);
     List<Doctor> findByLocation(Location location);
+
+//    List<Appointment> findByDoctorAndDate(Doctor doctor, LocalDate date);
 }
 
