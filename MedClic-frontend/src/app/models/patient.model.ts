1516f6d8a8fb8db86fbfc37e507faa7417711974
ilyashq <ilyasenaa24@gmail.com>
2024-10-06 22:59:12 +0100

    feat: Mettre à jour le modèle Patient pour refléter les modifications nécessaires

--- MedClic-frontend/src/app/models/patient.model.ts
+++ MedClic-frontend/src/app/models/patient.model.ts
@@ -2,6 +2,7 @@ import { Appointment } from './appointment.model';
 import { User } from './user.model';
 
 export interface Patient extends User {
+  has(id: number): unknown;
   medicalHistory: string;
   dateOfBirth: Date;
   insuranceInfo: string;
