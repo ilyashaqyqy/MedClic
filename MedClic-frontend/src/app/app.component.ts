a628f35763c052f75fb36a9d526a05793bc96a91
ilyashq <ilyasenaa24@gmail.com>
2024-10-08 11:21:26 +0100

    refactor: Améliorer la logique dans le composant app pour une structure plus claire

--- MedClic-frontend/src/app/app.component.ts
+++ MedClic-frontend/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthService } from './services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Medclic';
-}
+  
+    constructor(private authService: AuthService, private router: Router) {}
+  
+    isLoggedIn(): boolean {
+      return this.authService.isLoggedIn();
+    }
+  
+    // Check if the current route is home, admin dashboard, or doctor dashboard
+    shouldShowNavbar(): boolean {
+      const currentRoute = this.router.url;
+      return !(
+        currentRoute === '/home' ||
+        currentRoute.startsWith('/admin-dashboard') || 
+        currentRoute.startsWith('/doctor-dashboard') ||  
+        currentRoute.startsWith('/patient-dashboard')
+      );
+    }
+  }
